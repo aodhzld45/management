dc35e53a1bed095215ba511912e69787f3112fe7
hyunseok <prking94@naver.com>
2023-11-01 01:27:05 +0900

    2023-11-01 Customers REST API GET 형태로 호출하기 추가

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Customer from './components/customer';
+import React, { useState, useEffect } from 'react'; // React, useState, useEffect를 import
 
 // material-ui를 통한 테이블
 import Table from '@mui/material/Table';
-import { TableHead } from '@mui/material';
-import { TableBody } from '@mui/material';
-import { TableRow } from '@mui/material';
-import { TableCell } from '@mui/material';
-import { styled } from '@mui/system'; // styled 함수를 import합니다
+import { TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+import { styled } from '@mui/system'; // styled 함수를 import
 
 import { Paper } from '@mui/material';
 
@@ -21,41 +19,37 @@ const TableContainer = styled(Table)({
   minWidth: 1080,
 });
 
-const images = [
-  require("./img/ryan.jpg"),
-  require("./img/youkyung.jpg"),
-  require("./img/hong.jpg"),
-  // Add more image paths as needed
-];
-
-const customers = [
-  {
-    'id': 1,
-    'image': images[0],
-    'name': '서현석',
-    'birthday': '19941013',
-    'gender': '남자',
-    'job': '개발자',
-  },
-  {
-    'id': 2,
-    'image': images[1],
-    'name': '이유경',
-    'birthday': '19991105',
-    'gender': '여자',
-    'job': '가수',
-  },
-  {
-    'id': 3,
-    'image': images[2],
-    'name': '홍길동',
-    'birthday': '14430101',
-    'gender': '남자',
-    'job': '의적',
-  },
-];
+// const images = [
+//   require("./img/ryan.jpg"),
+//   require("./img/youkyung.jpg"),
+//   require("./img/hong.jpg"),
+//   // Add more image paths as needed
+// ];
 
 function App() {
+  // useState 훅을 사용하여 'customers' 상태와 그 상태를 업데이트할 'setCustomers' 함수를 생성
+  // 'customers'는 서버에서 불러온 고객 정보를 저장할 배열, 초기값은 빈 배열
+  const [customers, setCustomers] = useState([]);
+
+  // useEffect 훅을 사용하여 부수 효과(사이드 이펙트)를 처리
+  // 이 부분은 컴포넌트가 마운트될 때(fetch 요청 등) 실행하도록 설정
+  // 마운트 시 한 번 실행. 두 번째 매개변수인 빈 배열([])은 의존성 배열로, 여기서는 의존성이 없으므로 한 번만 실행
+  useEffect(() => {
+    callApi() // callApi 함수를 호출하여 고객 데이터를 불러옴
+      .then(res => setCustomers(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  // callApi 함수는 비동기 함수로, 서버에서 고객 정보를 가져오기 위해 API 요청
+  const callApi = async () => {
+    // fetch 함수를 사용하여 '/api/customers' 엔드포인트에 GET 요청
+    const response = await fetch('/api/customers');
+    // 서버에서 반환된 응답(response)을 JSON 형식으로 파싱하여 데이터를 추출
+    const body = await response.json();
+    // 파싱된 데이터(body)를 반환
+    return body;
+  };
+
   return (
     <RootContainer>
       <TableContainer>
@@ -72,7 +66,6 @@ function App() {
           {customers.map(c => (
             <Customer
               key={c.id}
-              id={c.id}
               image={c.image}
               name={c.name}
               birthday={c.birthday}
--- server.js
+++ server.js
@@ -1,16 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+// 이미지를 렌더링할 때 이미지 파일의 경로를 지정
+const images = [
+    '/img/ryan.jpg',
+    '/img/youkyung.jpg',
+    '/img/hong.jpg',
+  ];
+  
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('img')); // public/img 경로 설정
 
+
+// rest API 명세
 app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
+});
 
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': images[0],
+            'name': '서현석',
+            'birthday': '19941013',
+            'gender': '남자',
+            'job': '개발자',
+          },
+          {
+            'id': 2,
+            'image': images[1],
+            'name': '이유경',
+            'birthday': '19991105',
+            'gender': '여자',
+            'job': '가수',
+          },
+          {
+            'id': 3,
+            'image': images[2],
+            'name': '홍길동',
+            'birthday': '14430101',
+            'gender': '남자',
+            'job': '의적',
+          },
+        
+    ]);
 });
 
 app.listen(port, () => {
